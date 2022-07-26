4869ad2ef100614febea0a95ff13eff8ad438cf1
tusharsalunke1 <tusharsalunke1998@gmail.com>
2022-07-26 12:51:37 +0530

    user entity added

---
+++ github.com/tusharsalunke1/Event_management_system/demo/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,48 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int emaail;
+	private String add;
+	private int pincode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getEmaail() {
+		return emaail;
+	}
+	public void setEmaail(int emaail) {
+		this.emaail = emaail;
+	}
+	public String getAdd() {
+		return add;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+	public int getPincode() {
+		return pincode;
+	}
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+}
